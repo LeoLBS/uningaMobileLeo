b0f0325932f5e693f1d6cca8c05c8835d05726ae
LeoMLC <leonardoperin70@gmail.com>
2023-09-10 18:01:21 -0300

    Add MainActivity

--- app/src/main/java/com/example/uningamobile/MainActivity.java
+++ app/src/main/java/com/example/uningamobile/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.uningamobile;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button formButton;
+    private Button listButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        formButton = findViewById(R.id.formButton);
+        listButton = findViewById(R.id.listButton);
+        formButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+                 openFormActivity();
+        }
+        });
+
+        listButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openListActivity();
+        }
+        });
+    }
+
+    private void openFormActivity() {
+        Intent intent = new Intent(this, FormActivity.class);
+        startActivity(intent);
+    }
+
+    private void openListActivity() {
+       Intent intent = new Intent(this, ListActivity.class);
+       startActivity(intent);
     }
 }
